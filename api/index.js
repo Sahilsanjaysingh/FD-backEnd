74c2c43f19f0c800d65395b4307c0d2af5b9ff63
Sahilsanjaysingh <156179174+Sahilsanjaysingh@users.noreply.github.com>
2026-01-31 21:07:58 +0530

    fix vercel routing to api folder

--- api/index.js
+++ api/index.js
@@ -2,19 +2,19 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 
-import connectDb from "./config/db.js"
+import connectDb from "../config/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import http from "http"
 import { Server } from "socket.io"
 
-import authRouter from "./routes/auth.routes.js"
-import userRouter from "./routes/user.routes.js"
-import itemRouter from "./routes/item.routes.js"
-import shopRouter from "./routes/shop.routes.js"
-import orderRouter from "./routes/order.routes.js"
+import authRouter from "../routes/auth.routes.js"
+import userRouter from "../routes/user.routes.js"
+import itemRouter from "../routes/item.routes.js"
+import shopRouter from "../routes/shop.routes.js"
+import orderRouter from "../routes/order.routes.js"
 
-import { socketHandler } from "./socket.js"
+import { socketHandler } from "../socket.js"
 
 const app = express()
 
